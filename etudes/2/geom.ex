dd9950eea74547e59bc5b6d23d249c6a7c680c62
Mark <markdevilliers@gmail.com>
2014-04-25 13:49:50 +0100

    etudes 2_3 completed

--- etudes/2/geom.ex
+++ etudes/2/geom.ex
@@ -1,4 +1,18 @@
 defmodule Geom do
+	
+	@moduledoc """
+		Function for calculating geometric functions
+	"""
+
+	@vsn 0.1
+
+	@doc """
+		Calculates the area of a rectangle
+		All values default to 1
+	"""
+
+	@spec area(number(), number()) :: number()
+
 	def area(x \\ 1 ,y \\ 1) do
 		x * y
 	end
