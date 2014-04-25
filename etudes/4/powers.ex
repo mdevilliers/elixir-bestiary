bdaecd25a984fd9ebbbbe1239a52a8ced48566db
Mark <markdevilliers@gmail.com>
2014-04-25 16:27:32 +0100

    completed etudes 4_4

--- etudes/4/powers.ex
+++ etudes/4/powers.ex
@@ -1,7 +1,8 @@
 defmodule Powers do
 	
-	import Kernel, except: [raise: 2]
-
+	import Kernel, except: [raise: 2, raise: 3]
+    
+    @spec raise( number(), number()) :: number()
 	def raise(_, 0) do
 		1
 	end
@@ -18,4 +19,12 @@ defmodule Powers do
 		1.0 / raise(number, -power)
 	end
 
+	@spec raise( number(), number(), number()) :: number()
+	def raise(_, 0, acc) do
+		acc
+	end
+
+	def raise(number, power, acc) do
+		raise(number, power-1 , number * acc)
+	end
 end
