6ec91dca60e079f73e4c617b6e75322176c84be8
Mark <markdevilliers@gmail.com>
2014-04-25 14:20:26 +0100

    completed etudes 3_4

--- etudes/3/geom.ex
+++ etudes/3/geom.ex
@@ -10,22 +10,22 @@ defmodule Geom do
 		Calculates the area of a rectangle, triangle, ellipse
 	"""
 
-	@spec area(atom(), number(), number()) :: number()
-
+   @spec area({atom(), number(), number()}) :: number()
 	def area({type,x,y}) do
 		area(type, x, y )
 	end
 
-	defp area( :rectangle, x , y) when x >= 0 and y >= 0 do
+
+    @spec area(atom(), number(), number()) :: number()
+	defp area(:rectangle, x, y) when x >= 0 and y >= 0 do
 		x * y
 	end
 
-
-	defp area( :triangle, x , y) when x >= 0 and y >= 0 do
+	defp area(:triangle, x, y) when x >= 0 and y >= 0 do
 		(x * y) / 2.0
 	end
 
-	defp area( :ellipse, x , y) when x >= 0 and y >= 0 do
+	defp area(:ellipse, x, y) when x >= 0 and y >= 0 do
 		:math.pi() * x * y
 	end
 
