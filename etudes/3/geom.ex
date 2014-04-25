ac9e48ca7962015cb75338d77d3d77fbad4ff651
Mark <markdevilliers@gmail.com>
2014-04-25 14:06:37 +0100

    etudes 3_2 completed

--- etudes/3/geom.ex
+++ etudes/3/geom.ex
@@ -13,16 +13,16 @@ defmodule Geom do
 
 	@spec area(atom(), number(), number()) :: number()
 
-	def area( :rectangle, x , y) do
+	def area( :rectangle, x , y) when x >= 0 and y >= 0 do
 		x * y
 	end
 
 
-	def area( :triangle, x , y) do
+	def area( :triangle, x , y) when x >= 0 and y >= 0 do
 		(x * y) / 2.0
 	end
 
-	def area( :ellipse, x , y) do
+	def area( :ellipse, x , y) when x >= 0 and y >= 0 do
 		:math.pi() * x * y
 	end
 end
