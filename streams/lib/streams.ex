c935aa32954eef386c1353c8be2e3987ec2ea195
Mark deVilliers <markdevilliers@gmail.com>
2016-07-06 15:47:19 +0100

    implement stream

--- streams/lib/streams.ex
+++ streams/lib/streams.ex
@@ -10,8 +10,47 @@ defmodule Gateway do
     @endpoint
   end
 
+  defp process_url(@endpoint <> url) do
+    @endpoint <> url
+  end
+
   defp process_url(url) do
     @endpoint <> url
+  end
+
+end
+
+
+defmodule ResultStream do
+
+  def new(url) do
+    Stream.resource(
+      fn -> fetch_page(url) end,
+      &process_page/1,
+      fn _ -> end
+    )
+  end
+
+  defp fetch_page(url) do
+    response = Gateway.get!(url)
+    body = Poison.decode!(response.body)
+    items = body["data"]
+    next = body["links"]["next"]
+    {[items], next}
+  end
+
+  defp process_page({nil, nil}) do
+    {:halt, nil}
+  end
+
+  defp process_page({nil, next_page_url}) do
+    next_page_url
+    |> fetch_page
+    |> process_page
+  end
+
+  defp process_page({items, next_page_url}) do
+    {items, {nil, next_page_url}}
   end
 
 end
